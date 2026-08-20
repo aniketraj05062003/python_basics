81a1ea1eaaf03aa32ef7235cacc3c2ff9ce1b3c7
Aniket Raj <aniketyadav778203@gmail.com>
2026-08-21 00:02:53 +0530

    Add files via upload
    Demonstrates Python's built-in data types, including strings, integers, floats, complex numbers, dictionaries, booleans, sets, lists, and tuples, along with examples of using the type() function to inspect each data type

---
+++ data_type.py
@@ -0,0 +1,66 @@
+# data type
+
+# string data type
+name="rahul"    
+
+print(name)
+print(type(name))
+
+
+# int data type
+x=5
+
+print(x)
+print(type(x))
+
+
+# float data type
+x=5.45
+
+print(x)
+print(type(x))
+
+
+# complex data type
+x=5+4j
+
+print(x)
+print(type(x))
+
+
+# dis data type
+dis1={"name1":"ram"}
+
+print(dis1)
+
+print(type(dis1))
+
+# bool data type
+
+_pass=True
+
+print(_pass)
+
+print(type(_pass))
+
+# set data type
+
+set1={1,2,3}
+
+print(set1)
+print(type(set1))
+
+# list data type
+list1=["rahul","ram","rohit"]
+
+print(list1)
+print(type(list1))
+
+# tuple data type
+
+tuple1=(1,2,3,4)
+print(tuple1)
+print(type(tuple1))
+
+x=8
+print(x)
